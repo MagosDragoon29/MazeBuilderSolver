4076bdc463856d5815097c6d3fa63d14cef1729d
MagosDragoon29 <carpenter.evan29@gmail.com>
2024-09-17 16:28:53 -0400

    added line module to contain Point and Line classes, added draw_line to window

--- main.py
+++ main.py
@@ -5,3 +5,5 @@ from window import Window
 def main():
     win = Window(800, 600)
     win.wait_for_close()
+
+main()
--- window.py
+++ window.py
@@ -4,8 +4,30 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window:
     def __init__(self, width, height):
+        # root definition
         self.root = Tk()
         self.root.title ("Maze Solver")
+        
+        # Canvas Widget
         self.canvas = Canvas(self.root, width=width, height=height, bg="gray")
         self.canvas.pack(fill=BOTH, expand=True)
+        
+        self.running = False
+
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.root.update_idletasks()
+        self.root.update()
+
+    def wait_for_close(self):
+        self.running = True
+        while self.running:
+            self.redraw()
+
+    def close(self):
+        self.running = False
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
     
