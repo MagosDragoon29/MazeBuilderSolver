f67e51dd3cbcdc751c572146dcc5898df8406554
MagosDragoon29 <carpenter.evan29@gmail.com>
2024-09-19 17:31:04 -0400

    added draw_move method to Cell class in line module

--- line.py
+++ line.py
@@ -16,7 +16,7 @@ class Line:
         self.point2 = point2
     
     def draw(self, canvas, fill_color):
-        canvas.canvas.create_line(self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2)
+        canvas.create_line(self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2)
 
 
 
@@ -35,10 +35,19 @@ class Cell:
 
     def draw(self):
         if self.has_left_wall:
-            Line(Point(self._x1, self._y1), Point(self._x1, self._y2)).draw(self._win, 'black')
+            Line(Point(self._x1, self._y1), Point(self._x1, self._y2)).draw(self._win.canvas, 'black')
         if self.has_right_wall:
-            Line(Point(self._x2, self._y1), Point(self._x2, self._y2)).draw(self._win, 'black')
+            Line(Point(self._x2, self._y1), Point(self._x2, self._y2)).draw(self._win.canvas, 'black')
         if self.has_top_wall:
-            Line(Point(self._x1, self._y1), Point(self._x2, self._y1)).draw(self._win, 'black')
+            Line(Point(self._x1, self._y1), Point(self._x2, self._y1)).draw(self._win.canvas, 'black')
         if self.has_bottom_wall:
-            Line(Point(self._x1, self._y2), Point(self._x2, self._y2)).draw(self._win, 'black')
+            Line(Point(self._x1, self._y2), Point(self._x2, self._y2)).draw(self._win.canvas, 'black')
+    
+    def draw_move(self, to_cell, undo=False):
+        if undo:
+            color = 'gray'
+        else:
+            color = 'red'
+        start_point = Point((self._x1 + self._x2) /2, (self._y1 + self._y2) /2)
+        end_point = Point((to_cell._x1 + to_cell._x2)/2, (to_cell._y1 + to_cell._y2)/2)
+        Line(start_point, end_point).draw(self._win.canvas, color)
