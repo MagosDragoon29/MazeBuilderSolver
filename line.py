0c341db324ebcf19a0d14f7a5021b085cf207aa6
MagosDragoon29 <carpenter.evan29@gmail.com>
2024-09-19 17:04:13 -0400

    added Cell class to line module and finished the draw method for the Line class

--- line.py
+++ line.py
@@ -16,4 +16,29 @@ class Line:
         self.point2 = point2
     
     def draw(self, canvas, fill_color):
-        canvas.create_line(self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2)
+        canvas.canvas.create_line(self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2)
+
+
+
+
+class Cell:
+    def __init__(self, point1, point2, window):
+        self._x1 = point1.x
+        self._y1 = point1.y
+        self._x2 = point2.x
+        self._y2 = point2.y
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._win = window
+
+    def draw(self):
+        if self.has_left_wall:
+            Line(Point(self._x1, self._y1), Point(self._x1, self._y2)).draw(self._win, 'black')
+        if self.has_right_wall:
+            Line(Point(self._x2, self._y1), Point(self._x2, self._y2)).draw(self._win, 'black')
+        if self.has_top_wall:
+            Line(Point(self._x1, self._y1), Point(self._x2, self._y1)).draw(self._win, 'black')
+        if self.has_bottom_wall:
+            Line(Point(self._x1, self._y2), Point(self._x2, self._y2)).draw(self._win, 'black')
--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from window import Window
+from line import *
 
 # Line Intentionally Left Blank
 
 def main():
     win = Window(800, 600)
+    line1 = Line(Point(100, 100), Point(100, 200))
+    line2= Line(Point(200, 200), Point(200, 300))
+    #
+    point1 = Point(100, 100)
+    point2 = Point(200, 200)
+    cell1 = Cell(point1, point2, win)
+    cell1.draw()
+    #
     win.wait_for_close()
 
 main()
