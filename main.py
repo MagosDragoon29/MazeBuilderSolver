ae674974bba3ec06f5ef971ebeb9517a8ccb6ec8
MagosDragoon29 <carpenter.evan29@gmail.com>
2024-10-02 17:45:30 -0400

    completed the main function, it is default set to a size of 50 by 50, please feel free to adjust it for system performance

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from window import Window
 from line import *
 from maze import Maze
+import time
+import sys
 
 # Line Intentionally Left Blank
 
 def main():
     win = Window(800, 600)
-    line1 = Line(Point(100, 100), Point(100, 200))
-    line2= Line(Point(200, 200), Point(200, 300))
+    sys.setrecursionlimit(2000)
     #
-    m1 = Maze(0, 0,)
+    num_cols = 50
+    num_rows = 50
+    m1 = Maze(0, 0, num_rows, num_cols, 10, 10, win, seed = 29)
     #
+    m1._break_entrace_and_exit()
+    m1._break_walls_r(0,0)
+    time.sleep(2)
+    solved = m1.solve()
+    win.redraw()
     win.wait_for_close()
 
 main()
